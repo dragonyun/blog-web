ee7cab183941f5ae1b485ebe8b4e2d9649ace37d
yun <790878491@qq.com>
2022-04-19 20:46:50 +0800

    PC commit changes

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -4,7 +4,7 @@ import { Button } from 'antd';
 class Page extends Component {
 
     handleClick = () => {
-        console.log('123')
+        console.log('123456')
         const canvas = document.getElementById('tutorial');
         if(!canvas.getContext) return;
         //获得 2d 上下文对象
